f2ed203faa9aacf52a9fa6b69949570bf1a5088b
Mathbech <legeek.you@gmail.com>
2023-09-21 11:38:57 +0200

    Modification mineures

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -13,7 +13,7 @@ class PublicController extends AbstractController
     {
         return $this->render('public/index.html.twig', [
             'controller_name' => 'PublicController',
-            'controller_name2' => 'Filament gestion.fr',
+            'site_name' => 'Filament gestion.fr',
         ]);
     }
 }
