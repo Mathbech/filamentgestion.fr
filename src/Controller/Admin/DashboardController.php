698028865d1a302760a809f6a467cd43e311ef99
Mathbech <legeek.you@gmail.com>
2023-09-21 11:25:03 +0200

    Modification des icones du menu admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -12,7 +12,6 @@ use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +22,7 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-        $url = $routeBuilder->setController(BobinesCrudController::class)->generateUrl();
+        $url = $routeBuilder->setController(UsersCrudController::class)->generateUrl();
 
         return $this->redirect($url);
         // return parent::index();
@@ -56,14 +55,14 @@ class DashboardController extends AbstractDashboardController
     {
         // MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         //yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        yield MenuItem::linktoRoute('Retour a l\'accueil', 'fas fa-home', 'homepage');
+        yield MenuItem::linktoRoute('Retour a l\'accueil', 'fa fa-home', 'app_public');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa-solid fa-user', Users::class);
         yield MenuItem::linkToCrud('Bobines', 'fa-solid fa-boxes-stacked', Bobines::class);
         yield MenuItem::linkToCrud('Catégorie', 'fa-solid fa-folder-tree', Categorie::class);
         yield MenuItem::linkToCrud('Couleur', 'fa-solid fa-palette', Couleur::class);
-        yield MenuItem::linkToCrud('Consommation', 'fa-solid fa-dolly', Consommation::class);
-        yield MenuItem::linkToCrud('Impressions', 'fa-solid fa-print', Impressions::class);
         yield MenuItem::linkToCrud('Imprimantes', 'fa-solid fa-print', Imprimantes::class);
-        yield MenuItem::linkToCrud('Utilisateurs', 'fa-solid fa-print', Users::class);
+        yield MenuItem::linkToCrud('Impressions', 'fa-solid fa-print fa-beat-fade', Impressions::class);
+        yield MenuItem::linkToCrud('Consommation', 'fa-solid fa-dolly', Consommation::class);
     }
 
 }
