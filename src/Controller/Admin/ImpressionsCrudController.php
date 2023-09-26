8ee594d9bb3fdff7642ac3ee6c4a9efa714d43de
Mathbech <legeek.you@gmail.com>
2023-09-26 08:20:42 +0200

    Finalisation des assos crud

--- src/Controller/Admin/ImpressionsCrudController.php
+++ src/Controller/Admin/ImpressionsCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Impressions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 
 class ImpressionsCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class ImpressionsCrudController extends AbstractCrudController
         return Impressions::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TimeField::new('temps'),
+            DateTimeField::new('date'),
+            AssociationField::new('utilisateur')->autocomplete(),
         ];
     }
-    */
 }
