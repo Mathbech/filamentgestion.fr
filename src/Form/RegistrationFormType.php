b37d82a65635b6f567cdae4a6dd24359b388f37f
Mathbech <legeek.you@gmail.com>
2023-10-03 09:41:43 +0200

    Modification et stylisation de la page de register + ajout password confirm

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,38 +21,42 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, array('attr' => ['class' => 'form-control form-control-lg']))
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les termes d\'utilisation avant de continuer.',
                     ]),
                 ],
             ])
-            ->add('username')
-            ->add('plainPassword', PasswordType::class, [
+            ->add('username', TextType::class, array('attr' => ['class' => 'form-control form-control-lg']))
+            
+            ->add('Inscription', SubmitType::class, array('attr' => ['class' => 'btn btn-primary mr-2']))
+
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent correspondres.',
+                'options' => ['attr' => ['class' => 'form-control form-control-lg']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 8,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
             ]);
-            // ->add('plainPassword', RepeatedType::class, array(
-            //     'type' => PasswordType::class,
-            //     'first_options'  => array('label' => 'Password'),
-            //     'second_options' => array('label' => 'Repeat_Password'),
-            // ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
