608973fa626ad0a9d3af3201e79413ecc490f7c6
Mathbech <legeek.you@gmail.com>
2023-10-03 10:39:07 +0200

    modifications mineures

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -54,7 +54,6 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('dash_public'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
