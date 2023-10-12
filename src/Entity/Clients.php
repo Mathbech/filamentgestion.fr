6ea6836881e05df58a42dff5f0cdf6c809ff9954
Mathbech <legeek.you@gmail.com>
2023-10-12 11:32:45 +0200

    Ajout de tables pour les ventes et clients

---
+++ src/Entity/Clients.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ClientsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientsRepository::class)]
+#[ApiResource]
+class Clients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_inscription = null;
+
+    #[ORM\OneToMany(mappedBy: 'clients', targetEntity: Ventes::class)]
+    private Collection $ventes;
+
+    public function __construct()
+    {
+        $this->ventes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->date_inscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $date_inscription): static
+    {
+        $this->date_inscription = $date_inscription;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ventes>
+     */
+    public function getVentes(): Collection
+    {
+        return $this->ventes;
+    }
+
+    public function addVente(Ventes $vente): static
+    {
+        if (!$this->ventes->contains($vente)) {
+            $this->ventes->add($vente);
+            $vente->setClients($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVente(Ventes $vente): static
+    {
+        if ($this->ventes->removeElement($vente)) {
+            // set the owning side to null (unless already changed)
+            if ($vente->getClients() === $this) {
+                $vente->setClients(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Ventes.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\VentesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VentesRepository::class)]
+#[ApiResource]
+class Ventes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_produit = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description_produit = null;
+
+    #[ORM\Column]
+    private ?float $prix_produit = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_vente = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ventes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Clients $clients = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomProduit(): ?string
+    {
+        return $this->nom_produit;
+    }
+
+    public function setNomProduit(string $nom_produit): static
+    {
+        $this->nom_produit = $nom_produit;
+
+        return $this;
+    }
+
+    public function getDescriptionProduit(): ?string
+    {
+        return $this->description_produit;
+    }
+
+    public function setDescriptionProduit(string $description_produit): static
+    {
+        $this->description_produit = $description_produit;
+
+        return $this;
+    }
+
+    public function getPrixProduit(): ?float
+    {
+        return $this->prix_produit;
+    }
+
+    public function setPrixProduit(float $prix_produit): static
+    {
+        $this->prix_produit = $prix_produit;
+
+        return $this;
+    }
+
+    public function getDateVente(): ?\DateTimeInterface
+    {
+        return $this->date_vente;
+    }
+
+    public function setDateVente(\DateTimeInterface $date_vente): static
+    {
+        $this->date_vente = $date_vente;
+
+        return $this;
+    }
+
+    public function getClients(): ?Clients
+    {
+        return $this->clients;
+    }
+
+    public function setClients(?Clients $clients): static
+    {
+        $this->clients = $clients;
+
+        return $this;
+    }
+}
