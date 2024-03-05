3c25b3448ab7ec3193ef252afca100b374eb5a7c
Mathbech <legeek.you@gmail.com>
2024-03-05 10:13:16 +0100

    Mise à jour des dépendances

--- src/Controller/Admin/CouleurCrudController.php
+++ src/Controller/Admin/CouleurCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Couleur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class CouleurCrudController extends AbstractCrudController
 {
@@ -12,14 +14,13 @@ class CouleurCrudController extends AbstractCrudController
         return Couleur::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('Nom'),
         ];
     }
-    */
+    
 }
