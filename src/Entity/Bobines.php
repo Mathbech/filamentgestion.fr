d67826d5c484130987fff2336f23e800fa3c7c75
Mathbech <legeek.you@gmail.com>
2024-03-14 11:47:19 +0100

    Réorganisation des fichiers

--- src/Entity/Bobines.php
+++ src/Entity/Bobines.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BobinesRepository::class)]
-#[ApiResource(security: "is_granted('ROLE_USER')")]
+#[ApiResource]
 class Bobines
 {
     #[ORM\Id]
